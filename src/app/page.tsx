c80f496f585ff1dc736e45867bf05760074af0f9
Ifazo <ziaulkarimifaz@outlook.com>
2023-10-13 23:02:15 +0600

    testimonial, stats, tranding, upcoming added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import HomePage from '@/components/HomePage'
-import Image from 'next/image'
+import CategoryPreview from '@/components/CategoryPreview'
+import Hero from '@/components/Hero'
+import Newsletter from '@/components/Newsletter'
+import Stats from '@/components/Stats'
+import Testimonial from '@/components/Testimonial'
+import Trending from '@/components/Trending'
+import Upcoming from '@/components/Upcoming'
 
 export default function Home() {
   return (
     <main>
-      <HomePage />
+      <Hero />
+      <CategoryPreview />
+      <Trending />
+      <Upcoming />
+      <Testimonial />
+      <Stats />
+      <Newsletter />
     </main>
   )
 }
